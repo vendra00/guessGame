9849b7ca0ef46331517a24b2128754761645bce8
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2021-12-20 23:11:50 +0100

    Some comments on the Character.py

--- model/Character.py
+++ model/Character.py
@@ -3,8 +3,10 @@ from myEnums.PlayerClass import PlayerClass
 from myEnums.PlayerRace import PlayerRace
 
 
+# Class that represents a Character in the game
 class Character(object):
 
+    # Constructor with arguments
     def __init__(self, name: str = None, gender: Gender = None, player_class: PlayerClass = None,
                  player_race: PlayerRace = None, exp: int = 0):
         self.name = name
