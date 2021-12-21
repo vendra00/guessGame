ee38d399b0ed5cc9f4a3904f9f2434b2db0ded11
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2021-12-21 10:54:45 +0100

    Some tweeks in monster service and added coments on the methods

--- model/Monster.py
+++ model/Monster.py
@@ -4,10 +4,11 @@ from myEnums.MonsterType import MonsterType
 from myEnums.Size import Size
 
 
+# Class that represents a monster in the game
 class Monster(object):
 
     def __init__(self, name: Bestiary = None, monster_type: MonsterType = None, equipment: [Equipment] = None,
-                 size: Size = None, hp: int = None, attack: int = None, armor: int = 0, exp: int = 0):
+                 size: Size = None, hp: int = 0, attack: int = 0, armor: int = 0, exp: int = 0):
         self.name = name
         self.monster_type = monster_type
         self.size = size
--- service/MonsterService.py
+++ service/MonsterService.py
@@ -9,6 +9,7 @@ from myEnums.MonsterType import MonsterType
 from myEnums.Size import Size
 
 
+# Method that create and set a Monster in the game
 def generate_monster():
     monster: Monster = Monster.Monster()
     set_name(monster)
@@ -19,17 +20,17 @@ def generate_monster():
     set_attack(monster)
     set_armor(monster)
     set_exp(monster)
-    # name: Bestiary, monster_type: MonsterType, equipment: Equipment,
-    # size: Size, hp: int, attack: int, armor: int, exp: int
 
     print(monster.name.name, monster.monster_type.name, monster.size.name, monster.hp,
           monster.attack, monster.armor, monster.exp)
 
 
+# Method that set a name of a monster randomly from a list
 def set_name(monster: Monster):
     monster.name = random.choice(list(Bestiary))
 
 
+# Method that set a monster type from an enum
 def set_monster_type(monster: Monster):
     if monster.name.value == Bestiary(1).value or monster.name.value == Bestiary(3).value:
         monster.monster_type = MonsterType(1)
@@ -43,6 +44,7 @@ def set_monster_type(monster: Monster):
         monster.monster_type = MonsterType(7)
 
 
+# Method that sets a monster size from an enum
 def set_size(monster):
     if monster.name.value == Bestiary(2).value or monster.name.value == Bestiary(5).value \
             or monster.name.value == Bestiary(6).value or monster.name.value == Bestiary(8).value:
@@ -53,54 +55,59 @@ def set_size(monster):
         monster.size = Size(2)
 
 
+# Method that sets a monster equipment
 def set_equipment(monster):
     inventory = []
     # name: EquipmentName, placement: EquipmentPlacement, condition: int, armor: int
     if monster.name.value == Bestiary(4).value or monster.name.value == Bestiary(7).value:
         setType = take_armor_set()
-        inventory.append(Equipment(setType.Helmet.the_name, EquipmentPlacement(1).name, int(setType.Helmet.condition),
-                                   int(setType.Helmet.armor)))
-        inventory.append(Equipment(setType.Armor.the_name, EquipmentPlacement(5).name, int(setType.Armor.condition),
-                                   int(setType.Armor.armor)))
-        inventory.append(Equipment(setType.Bracers.the_name, EquipmentPlacement(6).name, int(setType.Bracers.condition),
-                                   int(setType.Bracers.armor)))
-        inventory.append(Equipment(setType.Bracers.the_name, EquipmentPlacement(7).name, int(setType.Bracers.condition),
-                                   int(setType.Bracers.armor)))
-        inventory.append(Equipment(setType.Boots.the_name, EquipmentPlacement(13).name, int(setType.Boots.condition),
-                                   int(setType.Boots.armor)))
-        inventory.append(Equipment(setType.Boots.the_name, EquipmentPlacement(14).name, int(setType.Boots.condition),
-                                   int(setType.Boots.armor)))
-        inventory.append(Equipment(setType.Shoulders.the_name, EquipmentPlacement(3).name, int(setType.Shoulders.condition),
-                                   int(setType.Shoulders.armor)))
-        inventory.append(Equipment(setType.Shoulders.the_name, EquipmentPlacement(4).name, int(setType.Shoulders.condition),
-                                   int(setType.Shoulders.armor)))
+        inventory.append(
+            Equipment(setType.Helmet.the_name, EquipmentPlacement(1), setType.Helmet.condition, setType.Helmet.armor))
+        inventory.append(
+            Equipment(setType.Armor.the_name, EquipmentPlacement(5), setType.Armor.condition, setType.Armor.armor))
+        inventory.append(
+            Equipment(setType.Bracers.the_name, EquipmentPlacement(6), setType.Bracers.condition, setType.Bracers.armor))
+        inventory.append(
+            Equipment(setType.Bracers.the_name, EquipmentPlacement(7), setType.Bracers.condition, setType.Bracers.armor))
+        inventory.append(
+            Equipment(setType.Boots.the_name, EquipmentPlacement(13), setType.Boots.condition, setType.Boots.armor))
+        inventory.append(
+            Equipment(setType.Boots.the_name, EquipmentPlacement(14), setType.Boots.condition, setType.Boots.armor))
+        inventory.append(
+            Equipment(setType.Shoulders.the_name, EquipmentPlacement(3), setType.Shoulders.condition, setType.Shoulders.armor))
+        inventory.append(
+            Equipment(setType.Shoulders.the_name, EquipmentPlacement(4), setType.Shoulders.condition, setType.Shoulders.armor))
         monster.equipment = inventory
 
 
+# Method that sets a monster hp based on his size
 def set_hp(monster):
     if monster.size.value == Size(1).value:
-        monster.hp = 50
+        monster.hp += 25
     elif monster.size.value == Size(2).value:
-        monster.hp = 100
+        monster.hp += 45
     elif monster.size.value == Size(3).value:
-        monster.hp = 200
+        monster.hp += 125
 
 
+# Method that sets a monster attack based on his size
 def set_attack(monster):
     if monster.size.value == Size(1).value:
-        monster.attack = 5
+        monster.attack += 5
     elif monster.size.value == Size(2).value:
-        monster.attack = 12
+        monster.attack += 12
     elif monster.size.value == Size(3).value:
-        monster.attack = 22
+        monster.attack += 22
 
 
+# Method that sets a monster armor based on his inventory
 def set_armor(monster):
     if monster.equipment != [None]:
         for equipment in monster.equipment:
             monster.armor += equipment.armor
 
 
+# Method that sets a monster experience
 def set_exp(monster):
     monster.exp = 100
 
